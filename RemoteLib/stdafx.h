f3f271ee23956296c4b1fd134b65fcc3d8edc44b
chrishoen <chris.hoen@red-inc.us>
2017-09-18 07:22:58 -0400

    added RemoteLib

---
+++ RemoteLib/stdafx.h
@@ -0,0 +1,44 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include <stdio.h>
+#include <string.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <math.h>
+#include <random>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/calib3d.hpp>
+#include <math.h>
+
+#include <iostream>
+#include <vector>
+#include <iterator>
+
+#include "my_functions.h"
+#include "prnPrint.h"
+#include "logFiles.h"
+#include "dsp_math.h"
+#include "risPortableCalls.h"
+
+#include <windows.h>
+
+#include <gdiplus.h> // Image from file stuff
+using namespace Gdiplus;
+#pragma comment (lib,"Gdiplus.lib")
+
+#include <mutex> // mutex in gdiwindowShow.cpp
+#include <thread> //Thread used for my tests with Spam image
+
+
+
+// TODO: reference additional headers your program requires here
